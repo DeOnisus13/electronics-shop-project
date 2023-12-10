e752f5a98ef8a485562db78a0285afadb60a50f5
Denis Antropov <de94onisus@gmail.com>
2023-12-11 00:47:27 +0400

    HW_1. Add class Item in src/item.py and add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,25 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def test_data():
+    item_1 = Item("Смартфон", 10000, 5)
+    item_2 = Item("Ноутбук", 25000, 3)
+    item_1.pay_rate = 0.8
+    item_2.pay_rate = 0.95
+    return item_1, item_2
+
+
+def test_calculate_total_price(test_data):
+    assert test_data[0].calculate_total_price() == 50000
+    assert test_data[1].calculate_total_price() == 75000
+
+
+def test_apply_discount(test_data):
+    test_data[0].apply_discount()
+    assert test_data[0].price == 8000
+
+    test_data[1].apply_discount()
+    assert test_data[1].price == 23750
