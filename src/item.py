eea9e1694a064205a74832d4c076e39439e0e921
Denis Antropov <de94onisus@gmail.com>
2023-12-19 00:11:54 +0400

    HomeWork_3

--- src/item.py
+++ src/item.py
@@ -37,6 +37,12 @@ class Item:
         else:
             self.__name = name[:10]
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.name}"
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
